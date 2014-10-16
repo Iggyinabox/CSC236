dbfbc31f8ccf501a313d0c80fb39ff19f03bdac3
Iggyinabox <fatcat71501@gmail.com>
2014-10-16 10:54:40 -0400

    Started Lab 3-B
    MEHHHH

---
+++ src/registrar/StudentRegistration.java
@@ -0,0 +1,47 @@
+/*
+ * Ignacio Lorenzo
+ * CSC 236-64
+ * Lab NumberHere
+ */
+
+/**
+ * Class StudenRegistration:
+ * public class StudentRegistration
+ *          The Student Registration class represents a record of a students
+ *          class registration. The class will know the student ID and 
+ *          and relate that ID to the classes they have registered
+ *          for.
+ * 
+ * Parameters:
+ * studentNode → a reference to the node that holds the student ID
+ * firstClassNode → a reference to the first class node
+ * 
+ * Specification:
+ * constructor for StudenRegistration
+ * public StudentRegistration()
+ * creates a empty StudenRegistration
+ * 
+ * public StudentRegistration(String id)
+ * creates a StudentRegistration with just the student not registered to
+ * any classes
+ * 
+ * Parameters:
+ * id → the student id number
+ * 
+ * Precondition: String > 0
+ * 
+ * public StudentRegistration(int id, String class, int section, int credits)
+ * creates a StudentRegistration with the student registered to one class
+ * 
+ * 
+ */
+
+package registrar;
+
+/**
+ *
+ * @author ilorenzo
+ */
+public class StudentRegistration {
+    
+}

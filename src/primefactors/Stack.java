9b528c61dd595bd0e5992349eb576a2d8d7efcf2
Iggyinabox <fatcat71501@gmail.com>
2014-10-30 13:50:21 -0400

    Last update to Stack
    ..

--- src/primefactors/Stack.java
+++ src/primefactors/Stack.java
@@ -6,10 +6,15 @@
 package primefactors;
 
 /**
- *
+ * An interface for a Stack
  * @author ilorenzo
  */
 public interface Stack<T> {
+    //Pushes a value into the collection
     void push(T value);
+    //Pops a element out of the collection and returns any value the 
+    //elemnt held
+    //Precondition: The stack cannot be empty
+    //Throws NoSuchElementException if stack is empty
     T pop();
 }
